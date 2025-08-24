f3271bb23cf2f7b30f7c80c05f390f1d50e7233b
Riyaad <riyaadhowlader@gmail.com>
2025-08-24 01:04:03 -0500

    database setup, active storage setup, devise setup.

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  has_many :follower_relationships, class_name: "Follow", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
+  has_many :following_relationships, class_name: "Follow", foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :following_relationships, source: :followed
+
+  has_one_attached :profile_image
+
+  validates :username, presence: true, uniqueness: true
+  validates :name, presence: true
+end
